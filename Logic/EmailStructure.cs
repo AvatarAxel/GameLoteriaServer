2c33aee9f2767b85aa2d2d809e312f779fecaf3a
Ale-VP <aavp1603@hotmail.com>
2022-11-21 17:49:10 -0600

    21.11.2022

--- Logic/EmailStructure.cs
+++ Logic/EmailStructure.cs
@@ -10,35 +10,41 @@ namespace Logic
 {
      public class EmailStructure
     {
-        public string sendMail(string to, string subject, string body)
+        public void sendMail(string to, string subject, string body)
         {
-            string message = "Error sending this email. Please verify the data or try again later";
             string from = "GameLoteriaUV@outlook.com";
             string displayName = "Game Loteria";
 
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(from, displayName);
+            mail.To.Add(to);
+
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient("smtp.office365.com", 587); 
+            client.Credentials = new NetworkCredential(from, "G@m3_UV_L0t3ri@");
+            client.EnableSsl = true;
+
             try
             {
-                MailMessage mail = new MailMessage();
-                mail.From = new MailAddress(from, displayName);
-                mail.To.Add(to);
-
-                mail.Subject = subject;
-                mail.Body = body;
-                mail.IsBodyHtml = true;
-
-                SmtpClient client = new SmtpClient("smtp.office365.com", 587); 
-                client.Credentials = new NetworkCredential(from, "G@m3_UV_L0t3ri@");
-                client.EnableSsl = true;
-
                 client.Send(mail);
-
             }
-            catch (Exception ex)
+            catch (ArgumentNullException)
             {
-                message = ex.Message + "Please verify your internet connection and that your data is correct and try again.";
+                /* 
+                 * Como mostrarle al usuario que no se pudo enviar el correo
+                */
             }
+            catch(ObjectDisposedException)
+            {
 
-            return message;
+            }
+            catch(SmtpFailedRecipientsException) 
+            {
+                
+            }
         }
     }
 }
---
+++ Logic/UserManager.cs
@@ -0,0 +1,160 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.ConstrainedExecution;
+using System.Net.NetworkInformation;
+
+
+namespace Logic
+{
+    public class UserManager
+    {
+        public Boolean RegisterUser(PlayerDTO player) 
+        {
+            bool status = false;
+            if (player == null)
+            {
+                using (var context = new GameLoteriaDataBasesEntities())
+                {
+                    if (!UserExist(player.Username, player.Email))
+                    {
+                        var newPlayerDB = context.player.Add(new player() { email = player.Email, username = player.Username, password = player.Password, coins = 500, birthday = player.Birthday });
+                        var resultado = context.SaveChanges();
+                        if (resultado > 0)
+                        {
+                            status = true;
+                        }
+                    }
+                }
+                return status;
+            }
+            return status;
+        }
+
+        public Boolean ValidationEmail(string email)
+        {
+            if (email == null)
+            {
+                using (var context = new GameLoteriaDataBasesEntities())
+                {
+                    var result = context.player.Where(x => x.email == email);
+                    if (result.Count() > 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                };
+            }
+            return false;
+        }
+
+        public Boolean ValidationUsername(string username)
+        {
+            if (username == null)
+            {
+                using (var context = new GameLoteriaDataBasesEntities())
+                {
+                    var result = context.player.Where(x => x.username == username);
+                    if (result.Count() > 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                };
+            }
+            return false;
+        }
+        public bool UserExist(string username, string email)
+        {
+            if (username == null || email == null)
+            {
+                using (var context = new GameLoteriaDataBasesEntities())
+                {
+                    var result = context.player.Where(x => x.username == username || x.email == email);
+                    if (result.Count() > 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                };
+            }
+            return false;
+        }
+        public PlayerDTO AuthenticationLogin(string username, string password)
+        {
+            PlayerDTO playerDTO = new PlayerDTO()
+            {
+                IsActive = false
+            };
+
+            if (username == null || password == null)
+            {
+                using (var context = new GameLoteriaDataBasesEntities())
+                {
+                    var players = (from Player in context.player where Player.username == username && Player.password == password select Player);
+
+                    if (players.Count() > 0)
+                    {
+                        playerDTO.Username = players.First().username;
+                        playerDTO.Email = players.First().email;
+                        playerDTO.IsActive = true;
+                        playerDTO.Coin = players.First().coins;
+                    }
+
+                }
+                return playerDTO;
+            }
+            return playerDTO;
+        }
+
+        public string ReceiveEmail(string EmailPlayers)
+        {
+            var random = new Random();
+            var valueContinue = random.Next(-10000,0);
+            string valueNext = valueContinue.ToString();
+
+            if (EmailPlayers == null)
+            {
+                var value = random.Next(0, 10000);
+
+                string verificationCode = value.ToString();
+
+                EmailStructure objLogic = new EmailStructure();
+                string body = "Hello player I enclose your verification code " + verificationCode;
+                objLogic.sendMail(EmailPlayers, " Verification Code ", body);
+
+                return verificationCode;
+            }
+            return valueNext;
+        }
+
+        public bool ChangePassword(string email, string password)
+        {
+            bool status = false;
+            if (email == null || password == null)
+            {
+                using (var context = new GameLoteriaDataBasesEntities())
+                {
+                    var player = context.player.Where(x => x.email == email).FirstOrDefault();
+                    if (player != null)
+                    {
+                        player.password = password;
+                        if (context.SaveChanges() > 0)
+                        {
+                            status = true;
+                        }
+                    }
+                }
+            }
+            return status;
+        }
+
+    }
+}
+
+
