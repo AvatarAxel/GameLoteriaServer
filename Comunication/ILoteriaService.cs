b08f261a0a4b010fc6ea84c5c07aa725380dcda7
Ale-VP <aavp1603@hotmail.com>
2022-12-05 15:44:36 -0600

    22.12.05

---
+++ Comunication/ILoteriaService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comunication
+{
+    [ServiceContract(CallbackContract = typeof(ILoteriaServiceCallBack))]
+    public interface ILoteriaService
+    {
+        [OperationContract]
+        void CreateLoteria(string verificationCode);
+        [OperationContract]
+        void DeleteLoteria(string verificationCode);
+        [OperationContract]
+        void JoinLoteria(string verificationCode);
+        [OperationContract]
+        void ExitLoteria(string verificationCode);
+        [OperationContract]
+        void ReciveWinner(string verificationCode);
+
+    }
+    [ServiceContract]
+    public interface ILoteriaServiceCallBack
+    {
+        [OperationContract(IsOneWay = true)]
+        void SendCard(int idCard);
+    }
+}
